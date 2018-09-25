894c5c1690b7d724e93fcbd3ffea493be7525a6c
Shurick <vaalex@tut.by>
2018-09-25 08:29:35 +0300

    test

---
+++ q.js
@@ -0,0 +1,15 @@
+function isPal(str, num, replace){
+    let newStr = str.split(" ")
+    for (let i in newStr) {
+
+
+        if(newStr[i].length >= num) {
+            newStr[i] = newStr[i].substring(0, num-1) + replace + newStr[i].substring(num)
+
+        }
+    }
+
+    return newStr.join(" ")
+}
+
+console.log(isPal("ffff                 f ff fff fffff        1", 3, "aa"))
